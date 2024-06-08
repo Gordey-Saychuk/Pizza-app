a4b663924c780443003924e160f40a4887f7d6be
Gordey-Saychuk <sajcukgordeil@gmail.com>
2024-06-08 21:52:07 +0300

    Outlet

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,9 @@
 import { ButtonProps } from "./Button.props";
-import "./Button.module.css";
+import styles from "./Button.module.css";
 import cn from 'classnames'
 
 export default function Button({children, className ,...props}: ButtonProps) {
   return (
-   <button className={cn("button accent", className)} {...props}>{children}</button>
+   <button className={cn(styles['button'], styles['accent'], className)} {...props}>{children}</button>
   )
 }
